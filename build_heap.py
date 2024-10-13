757e0d8468325d31c35fd2fcc64d2418b4c021f0
satvik_gs <satvik.gs@thoughtclan.com>
2024-10-13 13:36:59 +0530

    added heap code

--- build_heap.py
+++ build_heap.py
@@ -1,5 +1,31 @@
 
+
 class Heap:
-    list =[]
-    
-    
+    def __init__(self):
+        self.heap_list = [-1]
+    def insert(self, ele):
+        self.heap_list.append(ele)
+        
+        idx = len(self.heap_list) - 1
+        parent = idx //2
+        print("parent is",parent)
+        print("idx is",idx)
+        print(self.heap_list[idx])
+        while (idx>1 and self.heap_list[parent]> self.heap_list[idx] ):
+            self.heap_list[parent],self.heap_list[idx] = self.heap_list[idx], self.heap_list[parent]
+            
+            idx=parent
+            parent = idx//2
+            
+            
+my_heap = Heap()
+
+# Insert values into the heap
+my_heap.insert(5)
+my_heap.insert(3)
+my_heap.insert(7)
+my_heap.insert(1)
+my_heap.insert(9)
+
+# Print the heap to see the result
+print(my_heap.heap_list)
