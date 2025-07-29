847e03204798b89984275c9b54f2328fc8fb448b
gs7vik <1gssatvik@gmail.com>
2025-07-29 11:40:11 +0530

    modified callable

--- callable.py
+++ callable.py
@@ -7,3 +7,19 @@ res=x(2)
 print(res)
 
 #you use callable when you want an object to behave as a function 
+#this concept is used in decorators
+#for example
+
+class Decorator:
+    def __init__(self, func):
+        self.func = func
+    
+    def __call__(self,*args,**kwargs):
+        print(f"Calling {self.func.__name__}")
+        return self.func(*args, **kwargs)
+
+@Decorator
+def greet(name):
+    print(f"Hello, {name}")
+
+greet("Alice")
